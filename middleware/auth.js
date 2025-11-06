c5a9002500c31481d211f38b56a4f9ef0eb0dcc6
thaventhiran Ramsan <ramsandota@gmail.com>
2025-11-07 05:01:16 +0530

    file create

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import Reader from "../models/Reader.js";
+
+export const protect = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET || "yoursecret");
+
+      req.user = await Reader.findById(decoded.id).select("-password");
+      if (!req.user) throw new Error("User not found");
+
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  } else {
+    res.status(401).json({ message: "Not authorized, no token" });
+  }
+};
